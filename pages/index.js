84e6deae9c9b61878cc8323e7d1efcf67e36164e
Lianum Frada Bahesna <fbahezna@gmail.com>
2021-02-17 16:03:15 +0700

    add feature nprogress

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,7 @@ export default function Home({coinFilter}) {
 }
 
 export const getServerSideProps = async () => {
-  const res = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false')
+  const res = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=false')
 
   const coinFilter = await res.json()
 
